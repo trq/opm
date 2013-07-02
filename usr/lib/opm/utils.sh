fbe2c547aff80ddb094de1b87b9adb100205ad02
Tony R Quilkey <thorpe@thorpesystems.com>
2013-07-02 23:52:27 +1000

    Initial working prototype.

--- usr/lib/opm/utils.sh
+++ usr/lib/opm/utils.sh
@@ -7,3 +7,15 @@ die() { echo -e "\033[1;31m$@ \033[0m" 1>&2; exit 1; }
 msg() { echo -e "\033[1;32m$@ \033[0m"; }
 
 try() { msg "$@"; "$@" || die "Error running command: $@"; }
+
+source_file() {
+    local prefix="$1"
+    shift
+    for fn in "$@"
+    do
+        fn="$prefix/$fn"
+        [ ! -r "${fn}" ] && die "File $fn not found. Exiting."
+        msg "Sourcing ${fn} ..."
+        . "${fn}"
+    done
+}
