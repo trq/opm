4600749a87a6fb749d27318e2a38ea49109994c6
Tony R Quilkey <thorpe@thorpesystems.com>
2013-07-08 18:10:34 +1000

    Add help to help.

--- usr/lib/opm/usage.sh
+++ usr/lib/opm/usage.sh
@@ -1,5 +1,14 @@
 opm.usage() {
     echo "Usage:"
+    echo "  ${0} category/package-version <action>"
     echo
-    echo "  ${0} category/package-version action"
+    echo "Available actions:"
+    echo "  help        : show this help screen"
+    echo "  fetch       : download source archive(s) and patches"
+    echo "  unpack      : unpack sources (auto-dependencies if needed)"
+    echo "  configure   : configure sources (auto-fetch/unpack if needed)"
+    echo "  compile     : compile sources (auto-fetch/unpack/configure if needed)"
+    echo "  install     : install the package to the temporary install directory"
+    echo "  clean       : clean up all source and temporary files"
+    echo
 }
