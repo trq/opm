c009391dc15dcc7537eb56fc67fdc9334af7bd40
Tony R Quilkey <thorpe@thorpesystems.com>
2013-07-19 07:37:40 +1000

    Moved all the stage dep checking into the stage functions themselves.
    This enables us to execute all stages within the same environment. No
    longer do we need to source the package opm for each stage.

--- usr/lib/opm/main.sh
+++ usr/lib/opm/main.sh
@@ -17,52 +17,33 @@ opm.main() {
                 ;;
             fetch)
                 opm.fetch
-                opm.util.complete_stage fetch
                 ;;
             unpack)
-                opm.util.requires_stage fetch
                 opm.unpack
-                opm.util.complete_stage unpack
                 ;;
             prepare)
-                opm.util.requires_stage unpack
                 opm.prepare
-                opm.util.complete_stage prepare
                 ;;
             configure)
-                opm.util.requires_stage prepare
                 opm.configure
-                opm.util.complete_stage configure
                 ;;
             compile)
-                opm.util.requires_stage configure
                 opm.compile
-                opm.util.complete_stage compile
                 ;;
             preinstall)
-                opm.util.requires_stage compile
                 opm.preinstall
-                opm.util.complete_stage preinstall
                 ;;
             install)
-                opm.util.requires_stage preinstall
                 opm.install
-                opm.util.complete_stage 'install'
                 ;;
             postinstall)
-                opm.util.requires_stage 'install'
                 opm.postinstall
-                opm.util.complete_stage postinstall
                 ;;
             package)
-                opm.util.requires_stage postinstall
                 opm.package
-                opm.util.complete_stage package
                 ;;
             merge)
-                opm.util.requires_stage package
                 opm.merge
-                opm.util.complete_stage merge
                 ;;
             *)
                 opm.usage
