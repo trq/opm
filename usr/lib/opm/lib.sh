190af50701da8ac222ad1ecf02630c209c62b4dd
Tony R Quilkey <trq@thorpesystems.com>
2013-07-21 22:08:17 +1000

    Improvements to *info*

--- usr/lib/opm/lib.sh
+++ usr/lib/opm/lib.sh
@@ -11,14 +11,27 @@ opm.list() {
 }
 
 opm.info() {
+    echo "Package: $CATEGORY/$PACKAGE"
+    if [ -n "$description" ] ; then
+        local description=$(echo $description | fold -w80 -s -)
+        echo "Description:"
+        echo "  $description"
+        echo
+        echo "Sources:"
+        for url in "${sources[@]}"
+        do
+            echo "  $url"
+        done
+        echo
+    fi
     if [ -f ${METADIR}/${CATEGORY}/${PACKAGE}.installed ] ; then
-        echo "Package: $CATEGORY/$PACKAGE"
+        echo -e "\033[00;32mInstalled:\033[0m"
         echo "Install Date: $(date -r ${METADIR}/${CATEGORY}/${PACKAGE}.installed)"
         echo "Merged Files:"
         echo
         cat $METADIR/$CATEGORY/$PACKAGE.installed
     else
-        echo "$CATEGORY/$PACKAGE is not currently installed"
+        echo -e "\033[00;31mNot Installed\033[0m"
     fi
 }
 
